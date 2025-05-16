8a237e07ae311cf0fd4401e21c2eafa9dc10f03d
Kristoffer Aas <kristoffer@aas.dev>
2025-05-16 16:00:51 +0200

    F

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,10 +1,13 @@
 import { Onboarding } from "@/components/onboarding/onboarding";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="p-4 md:p-8">
       <div className="max-w-3xl mx-auto mt-16 mb-48">
-        <Onboarding />
+        <Suspense fallback={null}>
+          <Onboarding />
+        </Suspense>
       </div>
     </main>
   );
